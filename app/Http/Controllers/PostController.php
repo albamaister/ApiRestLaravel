46a4abb943dba7b4cb0e21dd6298346574f7b5e1
Andres Alba <bryanalba92@gmail.com>
2019-10-10 17:33:16 -0500

    Agregar y actualizar posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,41 +5,151 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Post;
+use App\Helpers\JwtAuth;
 
-class PostController extends Controller
-{
-    
-     public function __contructor() {
+class PostController extends Controller {
+
+    public function __contructor() {
         $this->middleware('api.auth', ['except' => ['index', 'show']]);
     }
-    
+
     public function index() {
-        
-        $posts = Post::all() -> load('category');
-        return response() -> json([
-            'code' => 200,
-            'status' => 'success',
-            'posts' => $posts
-        ], 200);
+
+        $posts = Post::all()->load('category');
+        return response()->json([
+                    'code' => 200,
+                    'status' => 'success',
+                    'posts' => $posts
+                        ], 200);
     }
-    
+
     public function show($id) {
-        $post = Post::find($id) -> load('category');
-        
-        if (is_object($post) ) {
+        $post = Post::find($id)->load('category');
+
+        if (is_object($post)) {
             $data = [
+                'code' => 200,
+                'status' => 'success',
+                'post' => $post
+            ];
+        } else {
+            $data = [
+                'code' => 404,
+                'status' => 'error',
+                'message' => 'La entrada no existe'
+            ];
+        }
+
+        return response()->json($data, $data['code']);
+    }
+
+    public function store(Request $request) {
+        // Recger los datos por post
+        $json = $request->input('json', null);
+        $params = json_decode($json);
+        $params_array = json_decode($json, true);
+
+        if (!empty($params_array)) {
+            // Conseguir el usuario identificado
+
+            $jwtAuth = new JwtAuth();
+            $token = $request->header('Authorization', null);
+            $user = $jwtAuth->checkToken($token, true);
+
+            // Validar los datos
+            // 
+            $validate = \Validator::make($params_array, [
+                        'title' => 'required',
+                        'content' => 'required',
+                        'category_id' => 'required',
+                        'image' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $data = [
+                    'code' => 400,
+                    'status' => 'error',
+                    'message' => 'No se a guardado el post, faltan datos'
+                ];
+            } else {
+
+
+                // Guardar el post (Articulo) 
+                $post = new Post();
+                $post->user_id = $user->sub;
+                $post->category_id = $params->category_id;
+                $post->title = $params->title;
+                $post->content = $params->content;
+                $post->image = $params->image;
+
+                $post->save();
+
+                $data = [
                     'code' => 200,
                     'status' => 'success',
                     'post' => $post
                 ];
+            }
         } else {
             $data = [
-                    'code' => 404,
-                    'status' => 'error',
-                    'message' => 'La entrada no existe'
-                ];
+                'code' => 400,
+                'status' => 'success',
+                'message' => 'Envia los datos correctamente'
+            ];
         }
-        
-        return response() -> json( $data, $data['code'] );
+
+        // Devolver la respuesta
+
+        return response()->json($data, $data['code']);
     }
+
+    public function update($id, Request $request) {
+        // Recoger los datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        // Datos para devolver
+        $data = [
+            'code' => 400,
+            'status' => 'error',
+            'message' => 'Datos enviados incorrectamente'
+        ];
+
+        if (!empty($params_array)) {
+            // Validar los datos
+
+            $validate = \Validator::make($params_array, [
+                        'title' => 'required',
+                        'content' => 'required',
+                        'category_id' => 'required'
+            ]);
+
+            if ($validate->fails()) {
+                $data['errors'] = $validate->errors();
+                return response()->json($data->data['code']);
+            }
+
+            // Quitar lo que no quiero actualizar
+
+            unset($params_array['id']);
+            unset($params_array['user_id']);
+            unset($params_array['created_at']);
+            unset($params_array['user']);
+
+
+            // Actualizar el registro en concreto
+
+            $post = Post::where('id', $id)->update($params_array);
+
+            $data = [
+                'code' => 200,
+                'status' => 'success',
+                'category' => $params_array
+            ];
+        }
+        // Devolver la respuesta los datos
+
+        return response()->json($data, $data['code']);
+    }
+
 }
